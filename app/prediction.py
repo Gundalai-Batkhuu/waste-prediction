cd531df883b49f333079c205ff62e25243a4deea
Gundalai-Batkhuu <gdbt0623@gmail.com>
2024-11-09 19:39:37 +1100

    Moved the model file

--- app/prediction.py
+++ app/prediction.py
@@ -1,6 +1,9 @@
 import joblib
+import os
 
 
 def predict(data):
-    clf = joblib.load('xgb_model.sav')
+    model_dir = os.path.join(os.path.dirname(__file__), 'models')
+    model_path = os.path.join(model_dir, 'xgb_model.sav')
+    clf = joblib.load(model_path)
     return clf.predict(data)
